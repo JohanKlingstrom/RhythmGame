15e0864ffbf80408b674864cd67dd33813bbe968
johk <johkli19@hotmail.se>
2022-06-06 17:14:24 +0200

    Dynamic loading for track maps

--- Bar.gd
+++ Bar.gd
@@ -2,34 +2,19 @@ extends Spatial
 
 var note_scn = preload("res://Note.tscn")
 
-var notes_data = [
-	{
-		"pos": 0,
-		#"len": 100
-	},
-	{
-		"pos": 400,
-		#"len": 100
-	},
-	{
-		"pos": 800,
-		#"len": 100
-	},
-	{
-		"pos": 1200,
-		#"len": 100
-	}
-]
-
 var note_scale # 0.005
+var bar_data
 
 func _ready():
 	add_notes()
 
 func add_notes():
-	for note_data in notes_data:
-		randomize()
-		var note = note_scn.instance()
-		note.line = (randi() % 3) + 1
-		note.position = int(note_data.pos)*note_scale
-		add_child(note)
+	var line = 1
+	for line_data in bar_data:
+		var notes_data = line_data.notes
+		for note_data in notes_data:
+			var note = note_scn.instance()
+			note.line = line
+			note.position = int(note_data.pos)*note_scale
+			add_child(note)
+		line += 1
